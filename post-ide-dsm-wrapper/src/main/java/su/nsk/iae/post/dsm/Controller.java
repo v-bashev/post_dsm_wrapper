68eded6af3985c44d69f2dd225a8bdd76a497696
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-03-31 18:22:35 +0700

    Update Controller.java

--- post-ide-dsm-wrapper/src/main/java/su/nsk/iae/post/dsm/Controller.java
+++ post-ide-dsm-wrapper/src/main/java/su/nsk/iae/post/dsm/Controller.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping(value = "hello")
+    @RequestMapping(value = "")
     public String hello(){
-        return "i'm alive";
+        return "Hello world!";
     }
 }
