c0e6deea826c21c318441f02b020ff4ef4b6fa80
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-04-16 15:27:13 +0700

    Обновил корневую библиотеку, поменял генератор на IDsmExecutor

--- post-ide-dsm-wrapper/src/main/java/su/nsk/iae/post/dsm/application/Executor.java
+++ post-ide-dsm-wrapper/src/main/java/su/nsk/iae/post/dsm/application/Executor.java
@@ -6,14 +6,14 @@ import java.util.Properties;
 import org.eclipse.xtext.generator.IGeneratorContext;
 import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
 import com.google.inject.Injector;
+import su.nsk.iae.post.IDsmExecutor;
 import su.nsk.iae.post.PoSTStandaloneSetup;
 import su.nsk.iae.post.dsm.domain.*;
-import su.nsk.iae.post.generator.IPoSTGenerator;
 
 public class Executor {
 
     public static String DSM_DIRECTORY;
-    public static String DSM_GENERATOR_CLASS_NAME;
+    public static String DSM_EXECUTOR_CLASS_NAME;
 
     public static String execute(DsmRequestDomain requestBody) {
         try {
@@ -24,7 +24,7 @@ public class Executor {
                     ", ast = " + requestBody.getAst()
             );
 
-            if (DSM_DIRECTORY == null || DSM_GENERATOR_CLASS_NAME == null) {
+            if (DSM_DIRECTORY == null || DSM_EXECUTOR_CLASS_NAME == null) {
                 readProperties();
             }
 
@@ -41,10 +41,10 @@ public class Executor {
             fsa.setOutputPath(generated);
             Logger.info(Executor.class, "output path: " + generated);
 
-            Logger.info(Executor.class, "creating generator...");
-            Class<?> clazz = Class.forName(DSM_GENERATOR_CLASS_NAME);
-            final IPoSTGenerator generator = (IPoSTGenerator) clazz.getConstructor().newInstance();
-            Logger.info(Executor.class, "successfully created generator of class " + generator.getClass().getName());
+            Logger.info(Executor.class, "creating executor...");
+            Class<?> clazz = Class.forName(DSM_EXECUTOR_CLASS_NAME);
+            final IDsmExecutor executor = (IDsmExecutor) clazz.getConstructor().newInstance();
+            Logger.info(Executor.class, "successfully created executor of class " + executor.getClass().getName());
 //            generator.beforeGenerate(resource, fsa, context);
 //            generator.doGenerate(resource, fsa, context);
 //            generator.afterGenerate(resource, fsa, context);
@@ -61,10 +61,10 @@ public class Executor {
         Properties properties = new Properties();
         properties.load(Executor.class.getClassLoader().getResourceAsStream("dsm.properties"));
         DSM_DIRECTORY = properties.getProperty("dsm.directory");
-        DSM_GENERATOR_CLASS_NAME = properties.getProperty("dsm.generatorClassName");
+        DSM_EXECUTOR_CLASS_NAME = properties.getProperty("dsm.executorClassName");
 
         Logger.info(Executor.class, "properties values:");
         Logger.info(Executor.class, "DSM_DIRECTORY = " + DSM_DIRECTORY);
-        Logger.info(Executor.class, "DSM_GENERATOR_CLASS_NAME = " + DSM_GENERATOR_CLASS_NAME);
+        Logger.info(Executor.class, "DSM_EXECUTOR_CLASS_NAME = " + DSM_EXECUTOR_CLASS_NAME);
     }
 }
