aa8bfaad13dae3b03482366a56b86b1f8fa7df16
v.bashev <vlad-498@yandex.ru>
2022-11-20 00:47:38 +0700

    Update core and api

--- post-ide-dsm-wrapper/src/main/java/su/nsk/iae/post/dsm/application/Executor.java
+++ post-ide-dsm-wrapper/src/main/java/su/nsk/iae/post/dsm/application/Executor.java
@@ -3,7 +3,14 @@ package su.nsk.iae.post.dsm.application;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Properties;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import su.nsk.iae.post.IDsmExecutor;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import su.nsk.iae.post.deserialization.ModelDeserializer;
 
 public class Executor {
     public static String DSM_EXECUTOR_CLASS_NAME;
@@ -15,10 +22,13 @@ public class Executor {
             }
             Logger.info(Executor.class, "all required properties are initialized");
             Logger.info(Executor.class, "creating executor...");
+            final String root = (String) request.get("root");
+            final String fileName = (String) request.get("fileName");
+            final Resource resource = ModelDeserializer.deserializeFromXMI((String) request.get("ast"));
             Class<?> clazz = Class.forName(DSM_EXECUTOR_CLASS_NAME);
             final IDsmExecutor executor = (IDsmExecutor) clazz.getConstructor().newInstance();
             Logger.info(Executor.class, "successfully created executor of class " + executor.getClass().getName());
-            return executor.execute(request);
+            return executor.execute(root, fileName, resource);
         } catch (Exception e) {
             Logger.error(Executor.class, e.getMessage());
             return "Error occurred: " + e.getMessage();
